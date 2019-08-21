2699f6005503757350d0d40bc88f2925e1c5e30c
M C <miguelcarmona996@gmail.com>
2019-08-21 20:14:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+var evens = [0, 2, 4, 6, 8, 10];
+
+function doToEvens(callback) {
+  evens.forEach(callback);
+}
+
+
 function changeCompletely(element, index, array) {
   array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 }
