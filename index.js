d6fdb6a56cb4a96d249b679394117dd423a71821
M C <miguelcarmona996@gmail.com>
2019-08-21 20:16:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var evens = [0, 2, 4, 6, 8, 10];
 for (var i = 0; i < evens.length; i++) {
   console.log(`${evens[i]} is not odd!`);
 }
+
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
+
+
 function changeCompletely(element, index, array) {
   array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 }
